7fb46ed4a907c372a9d424b384d7787343a8276d
YETA666 <429721783@qq.com>
2018-07-05 16:30:51 +0800

    提交之前遗漏的

--- src/main/java/com/yeta/sbl2/service/impl/FileServiceImpl.java
+++ src/main/java/com/yeta/sbl2/service/impl/FileServiceImpl.java
@@ -1,13 +1,20 @@
 package com.yeta.sbl2.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.yeta.sbl2.domain.MyResponse;
 import com.yeta.sbl2.service.FileService;
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.ss.usermodel.CellValue;
+import org.apache.poi.ss.usermodel.DataFormat;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
@@ -209,6 +216,134 @@ public class FileServiceImpl implements FileService {
             zipOutputStream.setComment("");
             zipOutputStream.flush();
             zipOutputStream.finish();
+        }
+    }
+
+    /**
+     * 创建Excel文件
+     * @param fileName
+     * @throws IOException
+     */
+    public static void writeExcel(String fileName) throws IOException {
+        //创建Excel工作簿
+        HSSFWorkbook workbook = new HSSFWorkbook();
+
+        //创建一个工作表sheet
+        HSSFSheet sheet = workbook.createSheet();
+
+        //添加数据
+        for (int i = 0; i < 10; i++) {
+            //创建一行
+            HSSFRow row = sheet.createRow(i);
+            //设置单元格宽度
+            for (int j = 0; j < 10; j++) {
+                //创建一个单元格
+                HSSFCell cell = row.createCell(j);
+                cell.setCellValue(j);
+            }
+        }
+
+        //创建文件
+        String path = ResourceUtils.getURL(ResourceUtils.CLASSPATH_URL_PREFIX).getPath();
+        File file = new File( path+ File.separator + "static/upload/" + fileName + "_" + System.currentTimeMillis() + ".xls");
+        file.createNewFile();
+
+        //将Excel写入到文件
+        FileOutputStream fos = FileUtils.openOutputStream(file);
+        workbook.write(fos);
+
+        //清理
+        fos.close();
+    }
+
+    /**
+     * 读取Excel文件
+     * @param fileName
+     * @throws Exception
+     */
+    public static void readExcel(String fileName) throws Exception {
+        //获取文件
+        File file = ResourceUtils.getFile(ResourceUtils.CLASSPATH_URL_PREFIX + "static/upload/" + fileName);
+
+        //判断文件是否存在
+        if (!file.exists()) {
+            throw new Exception("文件：" + fileName + "不存在！");
+        }
+
+        //通过文件创建Excel工作簿
+        FileInputStream fis = new FileInputStream(file);
+        HSSFWorkbook workbook = new HSSFWorkbook(fis);
+
+        //获取工作表sheet
+        HSSFSheet sheet = workbook.getSheetAt(0);
+
+        //读取数据
+        for (int i = 0; i < sheet.getLastRowNum(); i++) {
+            HSSFRow row = sheet.getRow(i);
+            for (int j = 0; j < row.getLastCellNum(); j++) {
+                HSSFCell cell = row.getCell(j);
+                Object value = null;
+                //判断单元格类型
+                switch (cell.getCellType()) {
+                    case HSSFCell.CELL_TYPE_NUMERIC: // 数字
+                        //如果为时间格式的内容
+                        if (HSSFDateUtil.isCellDateFormatted(cell)) {
+                            //注：format格式 yyyy-MM-dd hh:mm:ss 中小时为12小时制，若要24小时制，则把小h变为H即可，yyyy-MM-dd HH:mm:ss
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                            value = sdf.format(HSSFDateUtil.getJavaDate(cell.getNumericCellValue())).toString();
+                            break;
+                        } else {
+                            value = new DecimalFormat("0").format(cell.getNumericCellValue());
+                        }
+                        break;
+                    case HSSFCell.CELL_TYPE_STRING: // 字符串
+                        value = cell.getStringCellValue();
+                        break;
+                    case HSSFCell.CELL_TYPE_BOOLEAN: // Boolean
+                        value = cell.getBooleanCellValue() + "";
+                        break;
+                    case HSSFCell.CELL_TYPE_FORMULA: // 公式
+                        value = cell.getCellFormula() + "";
+                        break;
+                    case HSSFCell.CELL_TYPE_BLANK: // 空值
+                        value = "";
+                        break;
+                    case HSSFCell.CELL_TYPE_ERROR: // 故障
+                        value = "非法字符";
+                        break;
+                    default:
+                        value = "未知类型";
+                        break;
+                }
+                /*switch (cell.getCellTypeEnum()) {
+                    case _NONE:
+                        value = "_NONE";
+                        break;
+                    case BLANK:
+                        value = "BLANK";
+                        break;
+                    case BOOLEAN:
+                        value = cell.getBooleanCellValue();
+                        break;
+                    case ERROR:
+                        value = cell.getErrorCellValue();
+                        break;
+                    case FORMULA:
+                        value = cell.getCellFormula();
+                        break;
+                    case NUMERIC:
+                        value = cell.getNumericCellValue();
+                        break;
+                    case STRING:
+                        value = cell.getStringCellValue();
+                        break;
+                    default:
+                        value = "UNKNOW";
+                        break;
+                }*/
+                System.out.print(value + "\t");
+            }
+            System.out.println();
         }
     }
 }
